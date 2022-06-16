1a0365272960ed0902300295f863c2ad76587d93
emilhajizade42 <6xp3xcg@code.edu.az>
2022-06-16 11:07:24 +0400

    gdfhgshmhc

---
+++ project/WebApplication1/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Product 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public int DiscountPrecent { get; set; }
+        public string Description { get; set; }
+        public bool IsAvailabe { get; set; }
+
+    }
+}
